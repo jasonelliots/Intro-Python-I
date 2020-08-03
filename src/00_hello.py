f7c0f04dd23b80d633e6b58afd05f9da964fed10
jasonelliots <j.elliotschwartz@gmail.com>
2020-08-03 13:33:17 -0700

    finished exercises 0-7

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,10 @@
 # Print "Hello, world!" to your terminal
+
+print('Hello world')
+
+# name = "bobby"
+
+# if name != "sean":
+#     print('hey, you arent sean')
+# else:
+#     print(f'hello {name}')
--- src/05_lists.py
+++ src/05_lists.py
@@ -1,3 +1,4 @@
+
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
@@ -8,22 +9,63 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+
+x.append(4)
+
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+
+x = x + y
+
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
 
+print(len(x))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+for i in range(len(x)):
+    print(x[i]*1000)
+
+
+
+###################
+
+# empty = []
+
+# nums = [10,60,20,5]
+
+# # print(nums)
+
+# nums.append(77)
+
+# # print(nums)
+
+# # for elem in nums:
+# #     print(elem)
+
+# # for n in range(10):
+# #     print(n)
+
+# for i in range(len(nums)):
+#     print(i, nums[i])
+
+# for i, v in enumerate(nums):
+#     print(i, v)
